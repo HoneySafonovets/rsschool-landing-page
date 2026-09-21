73144845f6997ef56c6951c9fc36a684d7d9255f
HoneySafonovets <kfs.boi@yandex.ru>
2026-09-22 01:33:10 +0300

    feat: add comments

--- landing-page/landing-page/src/main.js
+++ landing-page/landing-page/src/main.js
@@ -1,7 +1,7 @@
 import './scss/main.scss';
 
-// alert('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
-// alert("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.")
+alert('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
+alert("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.")
 
 console.log('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
 console.log("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.");

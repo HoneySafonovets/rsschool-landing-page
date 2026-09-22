83881f385aa11e983a89fd283ebbe42caa0d27a1
HoneySafonovets <kfs.boi@yandex.ru>
2026-09-23 01:42:36 +0300

    fix: svg switchers

--- landing-page/landing-page/src/main.js
+++ landing-page/landing-page/src/main.js
@@ -1,10 +1,10 @@
 import './scss/main.scss';
 
-alert('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
-alert("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.")
+// alert('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
+// alert("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.")
 
-console.log('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
-console.log("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.");
+// console.log('Привет! Немного не успел доделать, пожалуйста, перепроверь ближе к дедлайну кросс-чека!');
+// console.log("Hi! I didn't quite manage to finish everything—please double-check it thoroughly.");
 
 const darkBtn = document.querySelector('.header__switch-dark');
 const lightBtn = document.querySelector('.header__switch-light');
--- landing-page/landing-page/vite.config.js
+++ landing-page/landing-page/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/rsschool-landing-page/landing-page/landing-page/',
+  base: './',
   build: {
     rollupOptions: {
       input: {
